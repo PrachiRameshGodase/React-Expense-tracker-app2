7e05dfe9fce0b89754de3578c90eef014108d9bb
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-15 17:48:42 +0530

    Adding first component ExpenseItem

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <ExpenseItem/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function ExpenseItem() {
+  return (
+    <div>
+      <h2>Expense item!</h2>
+      <h3>Food Rs 10</h3>
+      <h3>Petrol Rs 100</h3>
+      <h3>Movies Rs 200</h3>
+    </div>
+  )
+}
+
+export default ExpenseItem
