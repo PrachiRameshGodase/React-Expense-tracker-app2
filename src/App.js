b0af214f8c24113957e796f0d2a1b2c7beefe4b7
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-15 19:06:55 +0530

    Components Smaller and Readable

--- src/App.js
+++ src/App.js
@@ -35,10 +35,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started</h2>
-      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}/>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}/>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}/>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}/> */}
+      
       {expenses.map((expense)=>(
         <ExpenseItem title={expense.title}
          amount={expense.amount} 
