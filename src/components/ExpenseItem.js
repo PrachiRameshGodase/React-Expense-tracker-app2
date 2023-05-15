745304ea46486d817498dc75826ddb669493c3b5
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-15 19:07:13 +0530

    Components Smaller and Readable

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,16 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetails from './ExpenseDetails';
 import "./ExpenseItem.css"
 
 function ExpenseItem(props) {
   
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <h2>{props.location}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
+      
+      <ExpenseDetails amount={props.amount}  location={props.location} title={props.title} />
+      
     </div>
   )
 }
