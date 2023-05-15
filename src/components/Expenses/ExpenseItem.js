e25aca172f91f93c0606422542895022f9e5b386
PrachiRameshGodase <prachigodase2@gmail.com>
2023-05-15 20:36:16 +0530

    Adding a filter which doesnt work

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails';
 import Card from "../UI/Card";
@@ -6,26 +6,30 @@ import "./ExpenseItem.css"
 
 const ExpenseItem=(props) =>{
 
-  const [title,setTitle]=useState(props.title);
+  // const [title,setTitle]=useState(props.title);
 
-  const [amount,setAmount]=useState(props.amount);
+  // const [amount,setAmount]=useState(props.amount);
 
-  const clickHandler=()=>{
-    setTitle("Updated!");
-    console.log(title);
-  }
-  const deleteExpense=()=>{ 
-    setAmount("100")
-    console.log(amount);
-  }
+  // const clickHandler=()=>{
+  //   setTitle("Updated!");
+  //   console.log(title);
+  // }
+  // const deleteExpense=()=>{ 
+  //   setAmount("100")
+  //   console.log(amount);
+  // }
   
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       
-      <ExpenseDetails amount={amount}  location={props.location} title={title} />
+      {/* <ExpenseDetails amount={amount}  location={props.location} title={title} />
       <button onClick={clickHandler}>Change Title</button>
-      <button onClick={deleteExpense}>Delete Expense</button>
+      <button onClick={deleteExpense}>Delete Expense</button> */}
+      <ExpenseDetails 
+      title={props.title}  
+      location={props.location} 
+      amount={props.amount}/>
       
     </Card>  
   )
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ExpenseItem from './ExpenseItem'
 import Card from "../UI/Card";
+import ExpensesFilter from './ExpensesFilter';
 import "./Expenses.css"
 
 const Expenses=(props) =>{
+
+  const [filteredYear,setFilteredYear]=useState("2020")
+
+  const filterChangeHandler=selectedYear=>{
+    setFilteredYear(selectedYear);
+  }  
   return (
     <Card className='expenses'>
+      <ExpensesFilter 
+      selected={filteredYear} 
+      onChangeFilter={filterChangeHandler}/>
+      
       {props.items.map((expense)=>(
         <ExpenseItem 
         key={expense.id}
